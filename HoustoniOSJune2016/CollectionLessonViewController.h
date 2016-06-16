2841f80a67e72f43ddb5a7ed76196b95aec3e0ac
azamsharp <azamsharp@gmail.com>
2016-06-16 10:47:04 -0500

    Arrays and Loops
    Arrays and Loops (for, foreach) NSMutableArray and NSArray.

---
+++ HoustoniOSJune2016/CollectionLessonViewController.h
@@ -0,0 +1,13 @@
+//
+//  CollectionLessonViewController.h
+//  HoustoniOSJune2016
+//
+//  Created by Mohammad Azam on 6/16/16.
+//  Copyright © 2016 Mohammad Azam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CollectionLessonViewController : UIViewController
+
+@end
---
+++ HoustoniOSJune2016/CollectionLessonViewController.m
@@ -0,0 +1,75 @@
+//
+//  CollectionLessonViewController.m
+//  HoustoniOSJune2016
+//
+//  Created by Mohammad Azam on 6/16/16.
+//  Copyright © 2016 Mohammad Azam. All rights reserved.
+//
+
+#import "CollectionLessonViewController.h"
+
+@interface CollectionLessonViewController ()
+
+@end
+
+@implementation CollectionLessonViewController
+
+- (void)viewDidLoad {
+    
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    
+    // Create an NSArray
+    
+    // [DataType] [VariableName] = [Initializer]
+    
+    // NSArray *alphabets = [[NSArray alloc] init];
+    
+    // Create array with elements
+    NSArray *alphabets = [NSArray arrayWithObjects:@"A",@"B",@"C", nil];
+    
+    // NSMutableArray
+    
+   // NSMutableArray *alphabetsArray = [[NSMutableArray alloc] init];
+    
+    NSMutableArray *alphabetsArray = [NSMutableArray arrayWithObjects:@"A",@"B",@"C", nil];
+    
+    [alphabetsArray addObject:@"F"];
+    [alphabetsArray addObject:@"Q"];
+    
+    // Print the contents of the alphabetsArray
+    
+    // for loop in Objective-C
+    for(NSString *alphabet in alphabetsArray) {
+        
+        NSLog(@"%@",alphabet);
+    }
+    
+    // Other variation of for loop with indexes
+    
+//    for(int i = 0; i<alphabetsArray.count;i+=4) {
+//        
+//        NSLog(@"%@",alphabetsArray[i]);
+//        
+//    }
+    
+    
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
